5c30c6127a9225822a986190f115db387344ef58
Derek Batoyon <derek@bytemeister.com>
2019-12-09 16:55:35 -0800

    Day 4

---
+++ day04/part1.py
@@ -0,0 +1,44 @@
+# usage:
+#   python part1.py <lower bound>-<upper bound>
+# this produces all passwords matching criteria, count like:
+#   python part1.py 254032-789860 | wc -l
+
+import sys
+
+def convert(list):
+    result = 0
+    for x in list:
+        result = result * 10 + x
+    return result
+
+def repeating(g):
+    y = g[0]
+    for x in g[1:]:
+        if x == y:
+            return True
+        y = x
+
+def guess(digits, l=0, u=9):
+    if digits == 1:
+        for x in range(l, u+1):
+            yield [x]
+    else:
+        for x in range(l, u+1):
+            for g in guess(digits-1, x, u):
+                yield [x] + g
+
+def generate_guesses(digits=6):
+    for g in guess(digits):
+        if repeating(g):
+            yield convert(g)
+
+if __name__ == "__main__":
+    assert not repeating([1, 2, 3, 7, 8, 9])
+    assert repeating([1, 2, 3, 7, 7, 9])
+
+    (l, u) = sys.argv[1].split('-', 2)
+    lower = int(l)
+    upper = int(u)
+    for g in generate_guesses():
+        if g >= lower and g <= upper:
+            print g
---
+++ day04/part2.py
@@ -0,0 +1,54 @@
+# usage:
+#   python part2.py <lower bound>-<upper bound>
+# this produces all passwords matching criteria, count like:
+#   python part2.py 254032-789860 | wc -l
+
+import sys
+
+def convert(list):
+    result = 0
+    for x in list:
+        result = result * 10 + x
+    return result
+
+def correct_grouping(g):
+    r = []
+    y = g[0]
+    n = 1
+    for x in g[1:]:
+        if x == y:
+            n = n + 1
+        else:
+            r.append(n)
+            n = 1
+            y = x
+    #print convert(g), r + [n]
+    if n == 2 or 2 in r:
+        return True
+
+def guess(digits, l=0, u=9):
+    if digits == 1:
+        for x in range(l, u+1):
+            yield [x]
+    else:
+        for x in range(l, u+1):
+            for g in guess(digits-1, x, u):
+                yield [x] + g
+
+def generate_guesses(digits=6):
+    for g in guess(digits):
+        if correct_grouping(g):
+            yield convert(g)
+
+if __name__ == "__main__":
+    assert not correct_grouping([1, 2, 3, 7, 8, 9])
+    assert correct_grouping([1, 1, 2, 2, 3, 3])
+    assert not correct_grouping([1, 2, 3, 4, 4, 4])
+    assert correct_grouping([1, 1, 1, 1, 2, 2])
+
+    (l, u) = sys.argv[1].split('-', 2)
+    lower = int(l)
+    upper = int(u)
+    for g in generate_guesses():
+        if g >= lower and g <= upper:
+            print g
